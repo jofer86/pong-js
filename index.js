c0ed793b57edbbd18455bec28adc5900f66dec00
jofer86 <jofer86@gmail.com>
2019-10-16 23:16:23 -0500

    Add coments and update paddle Y position acording to mouse pos

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const canvas = document.getElementById('gameCanvas');
 const canvasContext = canvas.getContext('2d');
 
-// Ball Positions.
-
+// Ball Position
 let ballX = 75;
 let ballY = 75;
+// Ball Speed
 let ballSpeedX = 2;
 let ballSpeedY = 2;
+// Paddles Horizontal Position
+let paddle1Y = 250;
+const PADDLEHEIGHT = 100;
+
+// Edge of Screen Constants
 const [edge0, edgeX, edgeY] = [0, canvas.width, canvas.height]
 
 window.onload = () => {
   setInterval(moveAll, 1000 / 60);
+  // Calculates mouse position and updates the vert position of the paddle.
+  canvas.addEventListener('mousemove', e => {
+    let mousePos = calcMousePos(e);
+    paddle1Y = mousePos.y - (PADDLEHEIGHT / 2);
+  })
 }
 
 const drawCircle = (color, x, y, radius) => {
@@ -33,11 +43,11 @@ const drawEverything = () => {
   drawCircle('white', ballX, ballY, 10);
 
   // Left Paddle
-  drawRectangle('white', 0, 250, 10, 100)
-  
+  drawRectangle('white', 0, paddle1Y, 10, PADDLEHEIGHT)  
 }
 
 const moveEverything = () => {
+  // Detects when the ball hits an edge and makes it bounce.  
   if (ballX > edgeX) {
     ballSpeedX *= -1;
   }
@@ -53,6 +63,19 @@ const moveEverything = () => {
 
   ballX += ballSpeedX;
   ballY += ballSpeedY;
+}
+
+
+// Calculating mouse position in relation to the site scroll and play area
+const calcMousePos = e => {
+  let rect = canvas.getBoundingClientRect();
+  let root = document.documentElement;
+  let mouseX = e.clientX - rect.left - root.scrollLeft;
+  let mouseY = e.clientY - rect.top - root.scrollTop;
+  return {
+    x: mouseX,
+    y: mouseY
+  };
 }
 
 const moveAll = () => {
